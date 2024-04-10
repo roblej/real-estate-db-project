ec2a99ce5a90df138fccf9b93913c0d935b1ddb8
장태인 <80192692+roblej@users.noreply.github.com>
2024-04-10 11:46:16 +0900

    oracle_con.php 업데이트

--- oracle_con.php
+++ oracle_con.php
@@ -3,14 +3,14 @@
 //oracle database address
 $db ='(DESCRIPTION=
 (ADDRESS_LIST = 
-(ADDRESS = (PROTOCOL = TCP)(HOST = 203.249.87.57)(PORT = 1521)))
+(ADDRESS = (PROTOCOL = TCP)(HOST = )(PORT = )))
 (CONNECT_DATA = (SID = orcl)))';
 
 
 //enter user name & password
 
-$username = "S10_501";
-$password = "pw1234";
+$username = "";
+$password = "";
 
 // connect with oracle db
 $connect = oci_connect($username, $password, $db,'AL32UTF8');
